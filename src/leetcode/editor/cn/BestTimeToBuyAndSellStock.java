65ee71ddecad4abb8891d7b55152a69de0c38791
mqw <1315406616@qq.com>
2023-04-25 21:24:44 +0800

    2023-4-25

--- src/leetcode/editor/cn/BestTimeToBuyAndSellStock.java
+++ src/leetcode/editor/cn/BestTimeToBuyAndSellStock.java
@@ -47,17 +47,28 @@ class BestTimeToBuyAndSellStock {
         // dp[i]：前i天的最大收益
         public int maxProfit(int[] prices) {
 
-            int[] dp = new int[prices.length];
-            dp[0] = 0;
-            int min = prices[0];
-            for (int i = 1; i < prices.length; i++) {
-                if (min > prices[i]) {
-                    min = prices[i];
-                }
-                dp[i] = Math.max(dp[i - 1], prices[i] - min);
+   /*         if(prices.length == 1){
+                return 0;
+            }
+            // low记录前i个元素中个的最小值
+            int low = prices[0];
+            int max = 0;
+            for(int i = 1; i <prices.length; i++){
+                low = Math.min(low, prices[i]);
+                max = Math.max(max, prices[i] - low);
+            }
+            return max;*/
+
+            int[][] dp = new int[prices.length][2];
+            dp[0][0] = -prices[0];
+            dp[0][1] = 0;
+            for(int i = 1; i < prices.length; i++){
+                dp[i][0] = Math.max(dp[i-1][0], -prices[i]);
+                dp[i][1] = Math.max(dp[i-1][0] + prices[i], dp[i-1][1]);
             }
 
-            return dp[prices.length - 1];
+            return dp[dp.length-1][1];
+
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
--- src/leetcode/editor/cn/BestTimeToBuyAndSellStockIi.java
+++ src/leetcode/editor/cn/BestTimeToBuyAndSellStockIi.java
@@ -55,15 +55,15 @@ class BestTimeToBuyAndSellStockIi {
         // dp[i][1]：代表第i天结束时，手里持有股票的最大利润
         public int maxProfit(int[] prices) {
             int[][] dp = new int[prices.length][2];
-            dp[0][0] = 0;
-            dp[0][1] = -prices[0];
+            dp[0][0] = -prices[0];
+            dp[0][1] = 0;
 
-            for (int i = 1; i < prices.length; i++) {
-                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            for(int i = 1; i < prices.length; i++){
+                dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] - prices[i]);
+                dp[i][1] = Math.max(dp[i-1][0] + prices[i], dp[i-1][1]);
             }
 
-            return dp[prices.length - 1][0];
+            return dp[prices.length-1][1];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
